07afaf99d57e0766fb3a27b189470baf8a68ee51
Scott Bailey <csb5t@virginia.edu>
2017-10-12 12:34:59 -0700

    Add sample react component and folder structure for components

--- webpack/index.js
+++ webpack/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world');
+import React from 'react';
+import { render } from 'react-dom';
+import Hello from './components/Hello';
+
+const App = () => (
+  <Hello />
+);
+
+render(<App />, document.getElementById('hello'));
