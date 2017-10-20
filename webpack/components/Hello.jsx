12faf265a63a35fdbfe4677ab4c7174746cbc744
Scott Bailey <csb5t@virginia.edu>
2017-10-20 13:28:07 -0700

    Sketch out addition of redux

--- webpack/components/Hello.jsx
+++ webpack/components/Hello.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { setCurrentTime } from '../actionCreators';
 
-const Hello = () => <h1>Hello!</h1>;
+const Hello = props => (
+  <div>
+    <h1>{props.currentTime}</h1>
+    <input
+      onChange={props.handleTimeChange}
+      value={props.currentTime}
+      type="text"
+      placeholder="time"
+    />
+  </div>
+);
 
-export default Hello;
+Hello.propTypes = {
+  currentTime: PropTypes.string,
+  handleTimeChange: PropTypes.func
+};
+
+Hello.defaultProps = {
+  currentTime: '',
+  handleTimeChange: () => {}
+};
+
+const mapStateToProps = state => ({ currentTime: state.currentTime });
+const mapDispatchToProps = dispatch => ({
+  handleTimeChange(event) {
+    dispatch(setCurrentTime(event.target.value));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Hello);
--- webpack/index.jsx
+++ webpack/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import Hello from './components/Hello';
+import store from './store';
 
-const App = () => <Hello />;
+const App = () => (
+  <Provider store={store}>
+    <div className="app">
+      <Hello />
+    </div>
+  </Provider>
+);
 
 render(<App />, document.getElementById('hello'));
